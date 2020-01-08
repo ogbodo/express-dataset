4247aee96f2cf37d6dac52ddd99788bf0c0421b9
Izuking <izuking@okra.ng>
2020-01-08 13:44:50 +0100

    added route for erasing all events

--- routes/eraseEvents.js
+++ routes/eraseEvents.js
@@ -1,6 +1,21 @@
 var express = require('express');
 var router = express.Router();
+const contentType = require('../middlewares/validations')
+const eraseEventControllers = require('../controllers/events');
 
 // Route related to delete events
 
+//To delete all events
+router.delete('/', contentType, async (req, res) => {
+    try {
+        const response = await eraseEventControllers.eraseEvents();
+        if (!response.status) {
+            throw response;
+        }
+        res.json({ "status_code": 200, "body": {}, "headers": {} });
+    } catch (error) {
+        console.log(error);
+
+    }
+});
 module.exports = router;
