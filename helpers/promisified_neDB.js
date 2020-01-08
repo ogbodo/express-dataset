6893cc5e5c3411e9a524122f2732a9a7e0f66a54
Izuking <izuking@okra.ng>
2020-01-08 12:55:04 +0100

    promisified neDB methods for convenience

---
+++ helpers/promisified_neDB.js
@@ -0,0 +1,61 @@
+var DataStore = require('nedb');
+var DB = new DataStore();
+
+
+const insert = (data) => {
+    return new Promise((resolve, reject) => {
+        DB.insert(data, (err, doc) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(doc);
+            }
+        });
+    });
+}
+
+const find = (query = {}) => {
+
+    return new Promise((resolve, reject) => {
+        DB.find(query, (err, doc) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(doc);
+            }
+        });
+    });
+}
+
+const findOne = (query) => {
+    console.log('query', query);
+
+    return new Promise((resolve, reject) => {
+        DB.findOne(query, (err, doc) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve(doc);
+            }
+        });
+    });
+}
+
+const eraseAll = () => {
+
+    return new Promise((resolve, reject) => {
+        DB.remove({}, (err, doc) => {
+            if (err) {
+                reject(err);
+            }
+            else {
+                resolve({ status: true });
+            }
+        });
+    });
+}
+
+module.exports = { insert, find, findOne, eraseAll }
